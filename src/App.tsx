fc64db640bb7ae4486e6a62db6988e700a0565fb
spoonya <nomad8t@gmail.com>
2021-12-16 12:27:27 +0300

    fix: build error

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,14 @@ import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from './global-styles';
 import { themeDark, themeLight, themeConfig } from './constants/theme/theme.constant';
-import Project from './pages/Project.page';
 import useTypedSelector from './hooks/use-typed-selector.hook';
 
 export default function App(): React.ReactElement {
-  const { themeMode } = useTypedSelector(({ theme }) => theme);
+  const { themeMode } = useTypedSelector((store) => store.theme);
 
   return (
     <ThemeProvider theme={themeMode === themeConfig.light ? themeLight : themeDark}>
       <GlobalStyle />
-      <Project />
     </ThemeProvider>
   );
 }
